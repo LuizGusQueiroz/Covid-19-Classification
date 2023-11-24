1f46c8c84c9c4da83b8e9f6d4c2b681974003289
Luiz Gustavo <luizgusqueiroz@gmail.com>
2023-11-24 17:53:06 +0000

    ajustes resnet18

--- Resnet18_Covid19_Classification/Resnet18_Covid19_Classification.py
+++ Resnet18_Covid19_Classification/Resnet18_Covid19_Classification.py
@@ -121,7 +121,7 @@ def to_device(data, device):
     if isinstance(data, (list, tuple)):
         return [to_device(x, device) for x in data]
     else:
-        return data.to(device, non_blocking=True)
+        return data.to(device)
 
 
 class DeviceDataLoader:
